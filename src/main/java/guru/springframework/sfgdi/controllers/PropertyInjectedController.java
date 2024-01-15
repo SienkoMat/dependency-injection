0875d2bd6b594c0a67e5ead1646c6fe69c8c5cf1
SienkoMat <mateusz.sienko1@gmail.com>
2024-01-15 20:03:39 +0100

    Minor fixes.

--- src/main/java/guru/springframework/sfgdi/controllers/PropertyInjectedController.java
+++ src/main/java/guru/springframework/sfgdi/controllers/PropertyInjectedController.java
@@ -2,7 +2,7 @@ package guru.springframework.sfgdi.controllers;
 
 import guru.springframework.sfgdi.services.GreetingService;
 
-public class PropertyIncjectedController {
+public class PropertyInjectedController {
     public GreetingService greetingService;
     public String getGreeting() {
         return greetingService.sayGreeting();
